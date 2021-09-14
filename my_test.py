0061435a4d148e3ad90d804d33995cbc2affb743
foo1111 <jdimatteo+foo@gmail.com>
2021-09-14 13:28:35 -0600

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -4,9 +4,6 @@ class MyTest(unittest.TestCase):
 
     def test_passes(self):
         self.assertTrue(True) 
-        
-    def test_passes_again(self):
-        pass 
 
 
 if __name__ == '__main__':
